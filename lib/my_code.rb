d68f0fc46b588b309162dd0501bec9c4514f3eca
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 13:32:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ value = false
     if index == 1
       starting_point = 0
     else
-      yield (starting_point, source_array[index])
+      yield(starting_point, source_array[index])
     end
   end
 
