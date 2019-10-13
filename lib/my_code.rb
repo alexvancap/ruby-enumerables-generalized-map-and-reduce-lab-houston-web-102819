75f270f6c36636c6808189b3856584087502a844
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 13:40:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,7 @@ total = 0
     total = starting_point
   end
   while source_array[counter] do
-    pts yield(total, source_array[counter])
+    puts yield(total, source_array[counter])
     counter += 1
   end
 end
