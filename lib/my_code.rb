d5778574ed054ccecbed44bb3bda02c2961c78d0
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 13:05:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  counter = 0
+  while source_array[counter] do
+    new_array << yield(source_array[counter])
+    counter += 1
+  end
+  return new_array
+end
+
+
+
+def reduce(source_array, starting_point = 0)
+counter = 0
+total = starting_point / source_array.length
+boolean = true
+
+  while source_array[counter] do
+    total += source_array[counter]
+    counter += 1
+  end
+    return yield(total, source_array[counter])
+end
+
+source_array = [1, 2, 3, 4]
+p reduce(source_array){|memo, n| memo + n}
